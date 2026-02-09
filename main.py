45d879ef7448df13bc8425096c2789071b9f0f23
lg-civil <lgzuniga-urz@cps.edu>
2026-02-09 19:33:38 +0000

    task

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+# to-do list
 
+to_do_list = ["Homework", 'Walk the Dog', 'Eat Pizza']
+
+
+
+print("This is a list of things you need to finish!")
+print(to_do_list)
+to = input("Is there anything you want to add or remove?")
+
+
+if to == 'yes':
+    input("Add or remove?")
+else: 
+    print("Make sure to finish your current tasks and come back when you finish one to update your list!")
