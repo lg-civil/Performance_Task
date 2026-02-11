a74c403a0deb1b8787ff5eff7dcabfa6d9d7f20f
lg-civil <lgzuniga-urz@cps.edu>
2026-02-11 20:11:45 +0000

    lets gooo

--- main.py
+++ main.py
@@ -12,16 +12,29 @@ intro()
 def lists(to_do_list):
     answer = input("Is there anything you want to add or remove? (yes/no) ")
     if answer == "yes":
-        ans = input('Add or remove? ')
-        while ans == 'add' or 'Add':
-            a = input('What would you like to add?: ') 
-            to_do_list.append(a)
-            print(to_do_list)
-            input('Do you want to add something else? ')
-            if 'yes' or 'Yes':
-                continue
-            if 'no' or 'No':
-                break
+        ans = input('Add or remove? ').lower()
+        if ans == 'add':
+            while ans == 'add':
+                a = input('What would you like to add?: ') 
+                to_do_list.append(a)
+                print(to_do_list)
+                n = input('Do you want to add something else? ').lower()
+                if n == 'yes':
+                    continue
+                elif n == 'no':
+                    break
+                print('Ok go on and finish your list of things!')
+        elif ans == 'remove':
+            while ans == 'remove':
+                a = input('What would you like to remove?: ') 
+                to_do_list.remove(a)
+                print(to_do_list)
+                n = input('Do you want to remove something else? ').lower()
+                if n == 'yes':
+                    continue
+                elif n == 'no':
+                    break
+            print('Ok go on and finish your list of things!')
     else:
         print("Make sure to finish your current tasks and come back when you finish one to update your list!")
 
