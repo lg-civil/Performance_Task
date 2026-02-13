39693d6e32fc24134cfe1cda780a80e3545178d0
lg-civil <lgzuniga-urz@cps.edu>
2026-02-13 20:07:17 +0000

    problem somewhat solved

--- main.py
+++ main.py
@@ -18,23 +18,28 @@ def lists(to_do_list):
                 a = input('What would you like to add?: ') 
                 to_do_list.append(a)
                 print(to_do_list)
-                n = input('Do you want to add something else? ').lower()
+                n = input('Do you want to add something else? (if you wantr to remove something type the word "remove" ) ').lower()
                 if n == 'yes':
                     continue
                 elif n == 'no':
+                    print('Ok go on and finish your list of things!')
                     break
-                print('Ok go on and finish your list of things!')
+                elif n == 'remove':
+                    lists(to_do_list)
         elif ans == 'remove':
             while ans == 'remove':
-                a = input('What would you like to remove?: ') 
-                to_do_list.remove(a)
+                ab = input('What would you like to remove?: ') 
+                to_do_list.remove(ab)
                 print(to_do_list)
-                n = input('Do you want to remove something else? ').lower()
+                n = input('Do you want to remove something else? (if you want to add something type the word "add") ').lower()
                 if n == 'yes':
                     continue
                 elif n == 'no':
+                    print('Ok go on and finish your list of things!')
                     break
-            print('Ok go on and finish your list of things!')
+                elif n == 'add':
+                    lists(to_do_list)
+
     else:
         print("Make sure to finish your current tasks and come back when you finish one to update your list!")
 
@@ -44,4 +49,31 @@ lists(to_do_list)
 
 
 
-# def add_remove(to_do_list):
+# def add_remove():
+#     answer = input("Is there anything you want to add or remove? (yes/no) ").lower()
+#     if answer == 'yes':
+#         ans = input('Add or remove? ').lower()
+#         if ans == 'add':
+#             while ans == 'add':
+#                 a = input('What would you like to add?: ') 
+#                 to_do_list.append(a)
+#                 print(to_do_list)
+#                 n = input('Do you want to add something else? ').lower()
+#                 if n == 'yes':
+#                     continue
+#                 elif n == 'no':
+#                     break
+#             print('Ok go on and finish your list of things!')
+#         elif ans == 'remove':
+#             while ans == 'remove':
+#                 a = input('What would you like to remove?: ') 
+#                 to_do_list.remove(a)
+#                 print(to_do_list)
+#                 n = input('Do you want to remove something else? (if you want to add something say "add") ').lower()
+#                 if n == 'yes':
+#                     continue
+#                 elif n == 'no':
+#                     print('Ok go on and finish your list of things!')
+#                     break    
+#                 elif n == 'add':
+#                     add_remove()
